a5bd6a595ce4ba62dd654090be0cbc3d0631cb1a
fleurret <90931602+fleurret@users.noreply.github.com>
2026-09-22 19:06:20 -0700

    Update refilterch.m

--- helpers/refilterch.m
+++ helpers/refilterch.m
@@ -8,19 +8,33 @@ d = uigetfile_n_dir(fullfile(savedir, birdname, subf));
 for i = 1:length(d)
     
     % ch1 folder
-    cf = fullfile(d(i).folder, d(i).name, 'cmpJamm', 'Ch1');
+    
+    if length(d) > 1
+        cf = fullfile(d(i).folder, d(i).name, 'cmpJamm', 'Ch1');
+    else
+        cf = fullfile(d, 'cmpJamm', 'Ch1');
+    end
+    
     fnames = dir(cell2mat(fullfile(cf, '*.wav')));
-
+    fnames = fnames(~contains({fnames.name}, 'PostBlock'));
+    
     for j = 1:length(fnames)
         ffn = fullfile(fnames(j).folder, fnames(j).name);
         [wav, fs] = audioread(ffn);
-        Ch1 = wav(:,1);
-        Ch2 = wav(:,3);
-
-        filterCh1 = filterch(Ch1, Ch2);
-        wav(:,1) = filterCh1;
-
-        audiowrite(ffn, wav, fs)
+        
+        s = size(wav); 
+        
+        if s(2) == 5
+            continue
+        else
+            Ch1 = wav(:,1);
+            Ch2 = wav(:,2);
+            
+            filterCh1 = filterch(Ch1, Ch2);
+            nwav = [filterCh1, wav(:, 1:4)];
+            
+            audiowrite(ffn, nwav, fs)
+        end
     end
 end
 
